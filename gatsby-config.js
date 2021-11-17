27bb58e2c2692cd059808c34ce8a7dde99e855fa
Andrej Novikov <jazzaiman@gmail.com>
2021-11-17 22:28:55 +0200

    feat: background color meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,8 +43,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#20282b`,
+        theme_color: `#20282b`,
         display: `minimal-ui`,
         icon: `src/images/rizome-icon.png`, // This path is relative to the root of the site.
       },
